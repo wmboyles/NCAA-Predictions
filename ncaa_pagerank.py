96b62b75a38d411b1d0ef81d47267ce958dfe544
William Boyles <williamboyles22900@gmail.com>
2022-03-14 02:37:18 -0400

    Standardize team comparisons
    This will allow us to work with multiple comparison algorithms in a
    similar way.

--- ncaa_pagerank.py
+++ ncaa_pagerank.py
@@ -13,6 +13,8 @@ import matplotlib.pyplot as plt
 
 from enum import Enum
 
+from abc import ABC, abstractmethod
+
 
 class GameValues(Enum):
     """
@@ -57,17 +59,16 @@ class GameWeights(Enum):
     WEIGHTS = [50, 13.3333, 6.6666, 8.3333, 5]
 
 
-def rank(
-    year,
-    alpha=0.85,
-    iters=3500,
-    print_rankings=False,
-    plot_rankings=False,
-    serialize_results=True,
-    first_year=True,
-):
+class TeamComparator(ABC):
+    @abstractmethod
+    def compare_teams(self, teamA, teamB) -> float:
+        ...
+
+
+class PageRankComparator(TeamComparator):
     """
     Ranks all Division I NCAA Basketball teams in a given year using PageRank.
+
     PARAMETERS
     year -->    The year that the NCAA championship game takes place.
                 The 2019-2020 season would correpond to year=2020.
@@ -79,193 +80,204 @@ def rank(
     iters -->   The number of iterations of PageRank matrix multiplication to
                 perform. The default of iters=3500, about 10x the number of
                 Division I teams, is generally sufficient for ranking.
-    print_rankings -->  Should the function print out its rankings to stdout?
-                        Defaults to False.
-    plot_rankings -->   Should the function plot the rankings in sorted order?
-                        Defaults to False.
-    serialize_results -->   Should the function save its rankings to a .p file
-                            called "./predictions/[YEAR]/rankings.p"?
-                            Defaults to True.
     """
 
-    # Try to deserialize .p file summary. Try to create it if it doesn't exist
-    try:
-        total_summary = pickle.load(
-            open("./summaries/" + str(year) + "/total_summary.p", "rb")
-        )
-    except FileNotFoundError:
-        print("--- WARNING: No summary found for", year, "Trying to create summary...")
+    def __init__(self, year: int, alpha: float = 0.85, iters: int = 10000):
+        self.year = year
+        self.alpha = alpha
+        self.iters = iters
+
+        # Build PageRank stuff
+        self.__rank(serialize_results=True, first_year=True)
+        self.__build_model()
+
+    def __rank(self, **kwargs: dict[str, bool]):
+        """
+        Uses PageRank to create a vector ranking all teams.
+
+        KWARGS
+        print_rankings -->  Should the function print out its rankings to stdout?
+                            Defaults to False.
+        plot_rankings -->   Should the function plot the rankings in sorted order?
+                            Defaults to False.
+        serialize_results -->   Should the function save its rankings to a .p file
+                                called "./predictions/[YEAR]/rankings.p"?
+                                Defaults to True.
+        """
+
+        # Try to deserialize .p file summary. Try to create it if it doesn't exist
         try:
-            data_scraping.harvest(year)
-        except:
-            print("--- ERROR: Could not make summary for", year)
+            total_summary = pickle.load(
+                open(f"./summaries/{self.year}/total_summary.p", "rb")
+            )
+        except FileNotFoundError:
+            print(
+                f"--- WARNING: No summary found for {self.year} Trying to create summary..."
+            )
+            try:
+                data_scraping.harvest(self.year)
+            except:
+                print(f"--- ERROR: Could not make summary for {self.year}")
+                return
+
+            print(f"--- SUCCESS: Summary created for {self.year}")
+            print("--- Trying to rank again with newly created summary")
+
+            self.__rank(**kwargs)
             return
 
-        print("--- SUCCESS: Summary created for", year)
-        print("--- Trying to rank again with newly created summary")
-
-        rank(year, alpha, iters, print_rankings, plot_rankings, serialize_results)
-        return
-
-    # Get an ordered list of all the teams
-    teams = list(
-        set(
-            [
-                "-".join(game[GameValues.HOME_TEAM.value].split(" "))
-                for game in total_summary
-            ]
+        # Get an ordered list of all the teams
+        teams = list(
+            set(
+                [
+                    "-".join(game[GameValues.HOME_TEAM.value].split(" "))
+                    for game in total_summary
+                ]
+            )
         )
-    )
-    num_teams = len(teams)
+        num_teams = len(teams)
 
-    # Create PageRank matrix and initial vector
-    if first_year:
-        vec = np.ones((num_teams, 1))
-    else:
-        vec = rank(year - 1, alpha - 0.1, serialize_results=False, first_year=True)
+        # Create PageRank matrix and initial vector
+        if kwargs.get("first_year"):
+            vec = np.ones((num_teams, 1))
+        else:
+            self.year -= 1
+            self.alpha -= 0.1
 
-    num_teams = max(num_teams, len(vec))
-    mat = np.zeros((num_teams, num_teams))
-    for game in total_summary:
-        # We only want to count games where both teams are D1 (in teams list)
-        # We choose to only look at games where the first team won so we don't double-count games
-        if (
-            game[GameValues.HOME_TEAM.value] in teams
-            and game[GameValues.AWAY_TEAM.value] in teams
-            and game[GameValues.WIN_LOSS.value]
-        ):
-            # Game winner
-            # Since we know the first/home team won, we can already assign the weight for that
-            home_pr_score, away_pr_score = GameWeights.WEIGHTS.value[0], 0.0
+            kwargs["serialize_results"] = False
+            kwargs["first_year"] = True
+            vec = self.__rank(**kwargs)
 
-            # Effective field goal percentage
-            if game[GameValues.HOME_eFGp.value] > game[GameValues.AWAY_eFGp.value]:
-                home_pr_score += GameWeights.WEIGHTS.value[1]
-            elif game[GameValues.AWAY_eFGp.value] > game[GameValues.HOME_eFGp.value]:
-                away_pr_score += GameWeights.WEIGHTS.value[1]
+        num_teams = max(num_teams, len(vec))
+        mat = np.zeros((num_teams, num_teams))
+        for game in total_summary:
+            # We only want to count games where both teams are D1 (in teams list)
+            # We choose to only look at games where the first team won so we don't double-count games
+            if (
+                game[GameValues.HOME_TEAM.value] in teams
+                and game[GameValues.AWAY_TEAM.value] in teams
+                and game[GameValues.WIN_LOSS.value]
+            ):
+                # Game winner
+                # Since we know the first/home team won, we can already assign the weight for that
+                home_pr_score, away_pr_score = GameWeights.WEIGHTS.value[0], 0.0
 
-            # Turnover percentage
-            if game[GameValues.HOME_TOVp.value] < game[GameValues.AWAY_TOVp.value]:
-                home_pr_score += GameWeights.WEIGHTS.value[2]
-            elif game[GameValues.AWAY_TOVp.value] < game[GameValues.HOME_TOVp.value]:
-                away_pr_score += GameWeights.WEIGHTS.value[2]
+                # Effective field goal percentage
+                if game[GameValues.HOME_eFGp.value] > game[GameValues.AWAY_eFGp.value]:
+                    home_pr_score += GameWeights.WEIGHTS.value[1]
+                elif (
+                    game[GameValues.AWAY_eFGp.value] > game[GameValues.HOME_eFGp.value]
+                ):
+                    away_pr_score += GameWeights.WEIGHTS.value[1]
 
-            # Offensive rebound percentage
-            if game[GameValues.HOME_ORBp.value] > game[GameValues.AWAY_ORBp.value]:
-                home_pr_score += GameWeights.WEIGHTS.value[3]
-            elif game[GameValues.AWAY_ORBp.value] > game[GameValues.HOME_ORBp.value]:
-                away_pr_score += GameWeights.WEIGHTS.value[3]
+                # Turnover percentage
+                if game[GameValues.HOME_TOVp.value] < game[GameValues.AWAY_TOVp.value]:
+                    home_pr_score += GameWeights.WEIGHTS.value[2]
+                elif (
+                    game[GameValues.AWAY_TOVp.value] < game[GameValues.HOME_TOVp.value]
+                ):
+                    away_pr_score += GameWeights.WEIGHTS.value[2]
 
-            # Free throw rate
-            if game[GameValues.HOME_FTR.value] > game[GameValues.AWAY_FTR.value]:
-                home_pr_score += GameWeights.WEIGHTS.value[4]
-            elif game[GameValues.AWAY_FTR.value] > game[GameValues.HOME_FTR.value]:
-                away_pr_score += GameWeights.WEIGHTS.value[4]
+                # Offensive rebound percentage
+                if game[GameValues.HOME_ORBp.value] > game[GameValues.AWAY_ORBp.value]:
+                    home_pr_score += GameWeights.WEIGHTS.value[3]
+                elif (
+                    game[GameValues.AWAY_ORBp.value] > game[GameValues.HOME_ORBp.value]
+                ):
+                    away_pr_score += GameWeights.WEIGHTS.value[3]
 
-            # Add weighted score for this game to matrix for both teams
-            home_idx = teams.index(game[GameValues.HOME_TEAM.value])
-            away_idx = teams.index(game[GameValues.AWAY_TEAM.value])
+                # Free throw rate
+                if game[GameValues.HOME_FTR.value] > game[GameValues.AWAY_FTR.value]:
+                    home_pr_score += GameWeights.WEIGHTS.value[4]
+                elif game[GameValues.AWAY_FTR.value] > game[GameValues.HOME_FTR.value]:
+                    away_pr_score += GameWeights.WEIGHTS.value[4]
 
-            mat[home_idx, away_idx] += home_pr_score
-            mat[away_idx, home_idx] += away_pr_score
+                # Add weighted score for this game to matrix for both teams
+                home_idx = teams.index(game[GameValues.HOME_TEAM.value])
+                away_idx = teams.index(game[GameValues.AWAY_TEAM.value])
 
-    # Alter the matrix to take into account our alpha factor
-    mat = (alpha * mat) + (1 - alpha) * np.ones((num_teams, num_teams)) / num_teams
+                mat[home_idx, away_idx] += home_pr_score
+                mat[away_idx, home_idx] += away_pr_score
 
-    # Perform many iterations of matrix multiplication
-    for i in range(iters):
-        vec = mat @ vec
-        vec *= num_teams / sum(vec)  # Keep weights summed to set value (numerator)
+        # Alter the matrix to take into account our alpha factor
+        mat = (self.alpha * mat) + (1 - self.alpha) * np.ones(
+            (num_teams, num_teams)
+        ) / num_teams
 
-    # Sort the (ranking, team) pair into a list of tuples
-    sorted_pairs = sorted([(prob[0], team) for team, prob in zip(teams, vec)])
+        # Perform many iterations of matrix multiplication
+        for i in range(self.iters):
+            vec = mat @ vec
+            vec *= num_teams / sum(vec)  # Keep weights summed to set value (numerator)
 
-    # Print ranking pairs if specificed
-    if print_rankings:
-        for i in range(len(sorted_pairs)):
-            print(num_teams - i, sorted_pairs[i])
+        # Sort the (ranking, team) pair into a list of tuples
+        sorted_pairs = sorted([(prob[0], team) for team, prob in zip(teams, vec)])
 
-    # Serialize results if specificed
-    # TODO: Serialize results as dict?
-    if serialize_results:
-        # Make the year folder
-        outfile1 = f"./predictions/{year}_rankings.p"
-        outfile2 = f"./predictions/{year}_vector.p"
-        os.makedirs(os.path.dirname(outfile1), exist_ok=True)
+        # Print ranking pairs if specificed
+        if kwargs.get("print_rankings"):
+            for i in range(len(sorted_pairs)):
+                print(num_teams - i, sorted_pairs[i])
 
-        serial = dict()
-        for team in teams:
-            serial.setdefault(team, 0)
-        for item in sorted_pairs:
-            serial[item[1]] = item[0]
+        # Serialize results if specificed
+        if kwargs.get("serialize_results"):
+            # Make the year folder
+            outfile1 = f"./predictions/{self.year}_rankings.p"
+            outfile2 = f"./predictions/{self.year}_vector.p"
+            os.makedirs(os.path.dirname(outfile1), exist_ok=True)
 
-        pickle.dump(serial, open(outfile1, "wb"))
-        pickle.dump(vec, open(outfile2, "wb"))
+            serial = dict()
+            for team in teams:
+                serial.setdefault(team, 0)
+            for item in sorted_pairs:
+                serial[item[1]] = item[0]
 
-    # Plot graph of rankings if specified
-    if plot_rankings:
-        s = sorted(vec)
-        bins = np.arange(0.0, 3.5, 0.125)
-        hist, bins = np.histogram(s, bins=bins)
-        plt.hist(bins[:-1], bins, weights=hist)
-        plt.show()
+            pickle.dump(serial, open(outfile1, "wb"))
+            pickle.dump(vec, open(outfile2, "wb"))
 
-    return vec
+        # Plot graph of rankings if specified
+        if kwargs.get("plot_rankings"):
+            s = sorted(vec)
+            bins = np.arange(0.0, 3.5, 0.125)
+            hist, bins = np.histogram(s, bins=bins)
+            plt.hist(bins[:-1], bins, weights=hist)
+            plt.show()
 
+        return vec
 
-def compare_teams(
-    teamA: str,
-    teamB: str,
-    rankA: float,
-    rankB: float,
-    seedA: int,
-    seedB: int,
-    df: float,
-    min_vec: float,
-    max_vec: float,
-    print_out: bool = False,
-):
-    """
-    Compare two teams from the same year.
+    def __build_model(self):
+        # Get info about each team as attributes
+        self._rankings = pickle.load(
+            open(f"./predictions/{self.year}_rankings.p", "rb")
+        )
+        self._vec = pickle.load(open(f"./predictions/{self.year}_vector.p", "rb"))
+        self._df = chi2.fit(self._vec)[0]
+        self._min_vec, self._max_vec = min(self._vec)[0], max(self._vec)[0]
 
-    teamA: str
-        Name of first team to compare
-    teamB: str
-        Name of second team to compare
-    rankA: float
-        PageRank score of first team to compare
-    rankB: float
-        PageRank score of second team to compare
-    df: float
-        Degrees of freedom determined by chi squared model of all teams from year
-    min_vec: float
-        The minimum PageRank score of all teams from year
-    max_vec: float
-        The maximum PageRank score of all teams from year
-    print_out: bool
-        Prints "[Winning Team] beats [Losing Team]: [Confidence Level]" if true.
-        Defaults to false.
-    """
+    def compare_teams(self, teamA: str, teamB: str) -> float:
+        """
+        Compare two teams from the same year.
 
-    diff = (chi2.cdf(max_vec, df=df) - chi2.cdf(min_vec, df=df)) / sqrt(2)
-    a, b = chi2.cdf(rankA, df=df), chi2.cdf(rankB, df=df)
-    prob = min(abs(a - b) / diff + 0.5, 0.999)
+        Returns the probability that teamA will win.
+        """
 
-    if rankA >= rankB:
-        if print_out:
-            print(f"{teamA} ({seedA}) beats {teamB} ({seedB}): {prob}")
-        return prob
-    else:
-        if print_out:
-            print(f"{teamB} ({seedB}) beats {teamA} ({seedA}): {prob}")
-        return 1 - prob
+        rankA, rankB = self._rankings[teamA], self._rankings[teamB]
+
+        diff = (
+            chi2.cdf(self._max_vec, df=self._df) - chi2.cdf(self._min_vec, df=self._df)
+        ) / sqrt(2)
+        a, b = chi2.cdf(rankA, df=self._df), chi2.cdf(rankB, df=self._df)
+        prob = min(abs(a - b) / diff + 0.5, 0.999)
+
+        if rankA >= rankB:
+            return prob
+        else:
+            return 1 - prob
 
 
 def build_tourney(rankings: list) -> list:
     """
     Given a list of teams in ranked order, reorder them in NCAA bracket format.
     NCAA Bracket Format:
-    1) In any list of teams of length the #1 ranked team should play the #L ranked team in the first round
+    1) In any list of teams of length L the #1 ranked team should play the #L ranked team in the first round
     2) In any list of teams, the 1st and 2nd best teams shouldn't play each other until the final round.
     """
     if len(rankings) <= 1:
@@ -295,11 +307,7 @@ def build_tourney(rankings: list) -> list:
 
 
 def simulate_tourney(year: int, tourney: list) -> list:
-    rankings = pickle.load(open(f"./predictions/{year}_rankings.p", "rb"))
-    vec = pickle.load(open(f"./predictions/{year}_vector.p", "rb"))
-
-    df = chi2.fit(vec)[0]
-    min_vec, max_vec = min(vec)[0], max(vec)[0]
+    comparator: TeamComparator = PageRankComparator(year)
 
     rounds = [tourney]
     while len(tourney) > 1:
@@ -310,28 +318,18 @@ def simulate_tourney(year: int, tourney: list) -> list:
         for i in range(0, len(tourney), 2):
             # team name, seed in bracket, model's probability that they advance to current position
             teamA, seedA, prA = tourney[i]
-            rankA = rankings[teamA]
-
             teamB, seedB, prB = tourney[i + 1]
-            rankB = rankings[teamB]
 
-            A_beats_B = compare_teams(
-                teamA,
-                teamB,
-                rankA,
-                rankB,
-                seedA,
-                seedB,
-                df,
-                min_vec,
-                max_vec,
-                print_out=True,
-            )
+            A_beats_B = comparator.compare_teams(teamA, teamB)
             if A_beats_B >= 0.5:
+                print(f"{teamA} beats {teamB} ({A_beats_B})")
+
                 new_tourney.append((teamA, seedA, A_beats_B * prA))
                 if seedA > seedB:
                     print(f"\t{seedA} {seedB} UPSET")
             else:
+                print(f"{teamB} beats {teamA} ({1 - A_beats_B})")
+
                 new_tourney.append((teamB, seedB, (1 - A_beats_B) * prB))
                 if seedB > seedA:
                     print(f"\t{seedB} {seedA} UPSET")
