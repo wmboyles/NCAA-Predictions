fb961802dcc8c49d27cbceb21048f501258c9591
William Boyles <williamboyles22900@gmail.com>
2022-03-14 15:50:16 -0400

    Remove commented code to reconsile seeding algs
    I've decided to stick with our algorithm because it's more generalizable
    to different tournament sizes.

--- tournament.py
+++ tournament.py
@@ -63,21 +63,13 @@ class Tournament:
             """
             if size == 1:
                 yield 1
-                # return
-            else:
-                for seeding in get_seedings(size // 2):
-                    # # We need this dumb if statement to make our seeding ordering equivalent to what the NCAA uses
-                    # if (size == 4 and seeding == 2) or (
-                    #     size == 8 and seeding in {2, 3, 4}
-                    # ):
-                    #     yield size - seeding + 1
-                    #     yield seeding
-                    # else:
+                return
 
-                    # NOTE: For 16 teams, the output is [1, 16, 8, 9, 4, 13, 5, 12, 2, 15, 7, 10, 3, 14, 6, 11]
-                    # This is different from the NCAA's dumb printable bracket ordering.
-                    yield seeding
-                    yield size - seeding + 1
+            for seeding in get_seedings(size // 2):
+                # NOTE: For 16 teams, the output is [1, 16, 8, 9, 4, 13, 5, 12, 2, 15, 7, 10, 3, 14, 6, 11]
+                # This is different from the NCAA's dumb printable bracket ordering.
+                yield seeding
+                yield size - seeding + 1
 
         for i, seed in enumerate(get_seedings(num_teams // quadrants)):
             for quadrant in range(quadrants):
