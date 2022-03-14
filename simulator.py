4ffaa39277e956e8f7294e4f585b5d40f2e5f253
William Boyles <williamboyles22900@gmail.com>
2022-03-14 16:03:17 -0400

    Remove extra methods from simulator.py
    Although these methods weren't quite redundant, they were features that
    were not entirely necessary to the project. If we want them back, we can
    reimplement them.

--- simulator.py
+++ simulator.py
@@ -1,75 +1,10 @@
-import pickle
+"""
+This is the main script for the NCAA predictions project.
+One can select a comparator to compare teams and simulate a tournament.
+"""
 
 from comparison.team_comparators import PageRankComparator
 from comparison.tournament import Tournament
-
-
-def build_tourney(rankings: list) -> list:
-    """
-    Given a list of teams in ranked order, reorder them in NCAA bracket format.
-    NCAA Bracket Format:
-    1) In any list of teams of length L the #1 ranked team should play the #L ranked team in the first round
-    2) In any list of teams, the 1st and 2nd best teams shouldn't play each other until the final round.
-    """
-    if len(rankings) <= 1:
-        return rankings
-
-    left_rankings, right_rankings = [], []
-
-    # pointer to current tourney we're building
-    cur = left_rankings
-
-    # We start by adding the top team to the first half, then add the second and third teams to the second half
-    side_adds = 1
-    for seed in rankings:
-        cur.append(seed)
-
-        side_adds += 1
-
-        if side_adds >= 2:
-            if cur is left_rankings:
-                cur = right_rankings
-            else:
-                cur = left_rankings
-            side_adds = 0
-
-    tourney = build_tourney(left_rankings) + build_tourney(right_rankings)
-    return tourney
-
-
-def virtual_tourney(year: int) -> list:
-    """
-    Given a year, rank the top NUM_TEAMS teams according to our ranking system.
-    Arrange these teams based off this ranking into a NCAA bracket.
-    Simulate the bracket, predicting each matchup and the outcome with some confidence.
-    """
-
-    NUM_TEAMS = 64
-    NUM_SEEDS = 16
-    SEED_COUNT = NUM_TEAMS // NUM_SEEDS
-
-    try:
-        rankings = sorted(
-            pickle.load(open(f"./predictions/{year}_rankings.p", "rb")).items(),
-            key=lambda x: -x[1],
-        )[:NUM_TEAMS]
-
-        for i in range(NUM_SEEDS):
-            for j in range(SEED_COUNT):
-                # (team_name, seed, probability of getting to current point)
-                rankings[SEED_COUNT * i + j] = (
-                    rankings[SEED_COUNT * i + j][0],
-                    i + 1,
-                    1,
-                )
-
-        tourney = build_tourney(rankings)
-        return simulate_tourney(year, tourney)
-
-    except FileNotFoundError:
-        PageRankComparator(year)
-        return virtual_tourney(year)
-
 
 year = 2022
 comparator = PageRankComparator(year)
