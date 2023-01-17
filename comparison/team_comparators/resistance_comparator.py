71d073373fa3a24668c8a8d3470a93ba7ba4b8f1
William Boyles <williamboyles22900@gmail.com>
2023-01-16 20:38:46 -0800

    Clean up resistance comparator

--- comparison/team_comparators/resistance_comparator.py
+++ comparison/team_comparators/resistance_comparator.py
@@ -1,16 +1,22 @@
 import pickle
 import networkx as nx
 from collections import defaultdict
+from functools import reduce
+from operator import add
 
 from ..game_attrs import GameValues, TeamSeeding
 from .team_comparators import TeamComparator
 
 
 def resistance(G: nx.DiGraph) -> dict[tuple, float]:
+    """
+    Compute the resistance between all pairs of nodes in a weighted digraph.
+    """
+
     # Depth-first search to get a list of all paths from start to end
-    all_paths: list[list] = []
-    for start in G.nodes:
-        stack, paths = [(start, [start])], []
+    def find_all_paths(start):
+        stack: list[tuple] = [(start, [start])]
+        paths: list[list] = []
         while stack:
             (cur, path_to_cur) = stack.pop()
             for neighbor in set(G.neighbors(cur)):
@@ -18,29 +24,26 @@ def resistance(G: nx.DiGraph) -> dict[tuple, float]:
                     stack.append((neighbor, path_to_cur + [neighbor]))
             paths.append(path_to_cur)
 
-        # First path will just be [start], which we don't want
-        all_paths += paths[1:]
+        # paths[0] is the path from start to start, which we don't want
+        return paths[1:]
+
+    all_paths = reduce(add, (find_all_paths(start) for start in G.nodes))
 
     # Group paths by start and end node
     paths_by_start_end: dict[tuple, list] = defaultdict(list)
     for path in all_paths:
-        start, end = path[0], path[-1]
-        paths_by_start_end[(start, end)].append(path)
+        paths_by_start_end[(path[0], path[-1])].append(path)
 
     # For each start and end node, build a graph
-    graphs: dict[tuple, nx.Graph] = dict()
-    for start_end, paths in paths_by_start_end.items():
-        G_pair = nx.Graph()
-        for path in paths:
-            G_pair.add_weighted_edges_from(
-                (u, v, G[u][v]["weight"]) for (u, v) in zip(path, path[1:])
-            )
-
-        graphs[start_end] = G_pair
-
     # For each start_end pair graph, compute the resistance between the start and end nodes
     resistances: dict[tuple, float] = dict()
-    for (start, end), G_pair in graphs.items():
+    for (start, end), paths in paths_by_start_end.items():
+        G_pair = nx.Graph()
+        G_pair.add_weighted_edges_from(
+            (u, v, G[u][v]["weight"])
+            for path in paths
+            for (u, v) in zip(path, path[1:])
+        )
         resistances[(start, end)] = nx.resistance_distance(
             G_pair, start, end, weight="weight", invert_weight=True
         )
@@ -83,7 +86,10 @@ class ResistanceComparator(TeamComparator):
                     G.add_edge(teamA, teamB, weight=1)
 
         # Compute the resistance for all pairs of teams
-        resistances = resistance(G)
+        # NOTE: No need to invert weight, as nx.resistance_distance does this for us
+        resistances = defaultdict(dict)
+        for (start, end), ohms in resistance(G).items():
+            resistances[start][end] = ohms
 
         TeamComparator.serialize_results(year, "resistance", resistances, None)
 
@@ -93,7 +99,7 @@ class ResistanceComparator(TeamComparator):
         )
 
     def compare_teams(self, teamA: TeamSeeding, teamB: TeamSeeding) -> float:
-        R_AB = self._mat[(teamA.name, teamB.name)]
-        R_BA = self._mat[(teamB.name, teamA.name)]
+        R_AB = self._mat[teamA.name][teamB.name]
+        R_BA = self._mat[teamB.name][teamA.name]
 
         return R_BA / (R_AB + R_BA)
