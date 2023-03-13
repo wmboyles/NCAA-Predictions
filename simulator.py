802f4416599657baa58424e05d463ec60f6ce307
William Boyles <williamboyles22900@gmail.com>
2023-03-12 23:53:18 -0700

    Add NCAA tournament to simulator

--- simulator.py
+++ simulator.py
@@ -10,7 +10,79 @@ from comparison import Tournament
 from comparison.team_comparators import TeamComparator, COMPARATORS
 from visualization.bracket_generator import make_bracket
 
-tourney = Tournament.from_name_list([])
+# Seeds: [1, 16, 8, 9, 4, 13, 5, 12, 2, 15, 7, 10, 3, 14, 6, 11]
+tourney = Tournament.from_name_list(
+    [
+        # West
+        "kansas",
+        "howard",
+        "arkansas",
+        "illinois",
+        "connecticut",
+        "iona",
+        "saint-marys-ca",
+        "virginia-commonwealth",
+        "ucla",
+        "north-carolina-asheville",
+        "northwestern",
+        "boise-state",
+        "gonzaga",
+        "grand-canyon",
+        "texas-christian",
+        "arizona-state",  # play in vs nevada
+        # South
+        "alabama",
+        "southeast-missouri-state",  # play in vs texas-am-corpus-christi
+        "maryland",
+        "west-virginia",
+        "virginia",
+        "furman",
+        "san-diego-state",
+        "college-of-charleston",
+        "arizona",
+        "princeton",
+        "missouri",
+        "utah-state",
+        "baylor",
+        "california-santa-barbara",
+        "creighton",
+        "north-carolina-state",
+        # East
+        "purdue",
+        "fairleigh-dickinson",  # play in vs texas-southern
+        "memphis",
+        "florida-atlantic",
+        "tennessee",
+        "louisiana-lafayette",
+        "duke",
+        "oral-roberts",
+        "marquette",
+        "vermont",
+        "michigan-state",
+        "southern-california",
+        "kansas-state",
+        "montana-state",
+        "kentucky",
+        "providence",
+        # Midwest
+        "houston",
+        "northern-kentucky",
+        "iowa",
+        "auburn",
+        "indiana",
+        "kent-state",
+        "miami-fl",
+        "drake",
+        "texas",
+        "colgate",
+        "texas-am",
+        "penn-state",
+        "xavier",
+        "kennesaw-state",
+        "iowa-state",
+        "pittsburgh",  # play in vs mississippi-state
+    ]
+)
 
 
 def main(year: int, comparator_type):
